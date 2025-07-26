1f7f860aa06a5a9cc4f4e025cf82fe843b304224
Pepe2795 <jarevalobezic@gmail.com>
2025-07-26 18:44:19 -0300

    Create unique_users_per_garage_shared.sql

---
+++ sql/unique_users_per_garage_shared.sql
@@ -0,0 +1,8 @@
+--number of unique individuals that use each garage’s shared charging stations. 
+
+SELECT	garage_id,
+		COUNT(DISTINCT user_id) AS num_unique_users
+	FROM charging_sessions
+WHERE user_type = 'Shared'
+GROUP BY garage_id
+ORDER BY num_unique_users DESC
